31356454218d1c251752269d2402712a645765cd
abdelmonem salem <abdelmonem.salem.as@gmail.com>
2021-05-23 14:56:15 +0300

    Create main Components

---
+++ src/Components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            Plotter
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </header>
+    );
+}
+ 
+
+export default Header;
