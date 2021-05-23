4ef1e3e43964de37e6e8b492b7624f54d249ce9c
abdelmonem salem <abdelmonem.salem.as@gmail.com>
2021-05-23 15:16:20 +0300

    Get and show Data / Drag, Drop, Clear Functions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'reset-css';
 import Header from './Components/header'
 import Columns from './Components/columns';
 import Inquiry from './Components/inquiry';
-import LineChart from './Components/chart'
 import {GetColumns, GetItem} from './API/GetData';
 
 class App extends Component {
@@ -16,39 +15,97 @@ class App extends Component {
             details: []
         };
       }
-    componentDidMount() {
-
+      componentDidMount() {
+        GetColumns()
+        .then(response => {
+          this.setState({columns: response.data})
+        })
+        .catch(err => {
+            console.log(err)
+        });
     }
 
     handleDragStart = (e, name) => {
-        
+        e.dataTransfer.setData("name", name)
     }
         
     handleDragOver = e => {
-        
+        e.preventDefault();
     }
         
     getDetails = (inquiry) => {
-        
+        let measures = [];
+        let dimension = []
+        inquiry.filter(item => item.function === 'measure').map(item => measures.push(item.name));
+        inquiry.filter(item => item.function === 'dimension').map(item => dimension.push(item.name));
+        let data = {"measures": measures, "dimension": dimension[0]};
+        if (measures.length > 0 && dimension.length > 0) {
+            GetItem(data)
+            .then(response => {
+                this.setState({details: response.data})
+            })
+            .catch(err => {
+                console.log(err)
+            });
+        }
     }
 
     handleDrop = (e) => {
+        const {columns, inquiry} = this.state;
+        let name = e.dataTransfer.getData("name");
+        let columnItem = columns.filter(column => {
+            return column.name !== name;
+        });
+        let inquiryItem = columns.filter(column => {
+            return column.name === name;
+        });
+        this.setState({columns: columnItem});
 
+        if(inquiryItem[0].function !== 'dimension') {
+            let inquiryItems = [...inquiry, inquiryItem[0]]
+            this.getDetails(inquiryItems)
+            this.setState({inquiry: inquiryItems});
+        } else {
+            this.setState({columns: [...columnItem, ...inquiry.filter(item => {
+                return item.function === 'dimension';
+            })]});
+            let inquiryItems = [...inquiry.filter(item => {
+            return item.function !== 'dimension';
+            }), inquiryItem[0]]
+            this.getDetails(inquiryItems)
+            this.setState({inquiry: inquiryItems});
+        }
     }
         
     handleClear = (type) => {
-        
+        const {columns, inquiry} = this.state;
+        if (type === 'dimension') {
+            this.setState({columns: [...columns, ...inquiry.filter(item => {
+                return item.function === 'dimension';
+            })]});
+            this.setState({inquiry: [...inquiry.filter(item => {
+                return item.function !== 'dimension';
+            })]});
+        } else {
+            this.setState({columns: [...columns, ...inquiry.filter(item => {
+                return item.function === 'measure';
+            })]});
+            this.setState({inquiry: [...inquiry.filter(item => {
+                return item.function !== 'measure';
+            })]});
+        }
+        this.setState({details: []})
     }
 
     render() {
-        const {columns, inquiry, details} = this.state;
+        const {columns, inquiry} = this.state;
         return (
             <div className="container">
                 <Header />
                 <Columns columns={columns} handleDragStart={this.handleDragStart} />
                 <div className="rightCol">
                     <Inquiry inquiry={inquiry} handleDragOver={this.handleDragOver} handleDrop={this.handleDrop} handleClear={this.handleClear} />
-                    {details.length > 0 ? <LineChart details={details} /> : ''}
+                    
                 </div>
             </div>
         )
